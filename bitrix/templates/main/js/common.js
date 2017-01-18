1634e77770d9f4bd0b18e4bf74252802b563350d
gitwhatislove <whatislove0505@gmail.com>
2017-01-18 16:37:15 +0300

    'add_page_about_and_price'

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	//init map
-	ymaps.ready(initYandexMap);
+	
 
 	//init slider
 	$('.slider-init').slick({
@@ -20,6 +20,25 @@ $(document).ready(function() {
 		]		
 	});
 
+	$('.slider-init-2_1').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		asNavFor: '.slider-init-2_2',
+		prevArrow: '<button type="button" class="slider_arrow slider_arrow-pre"><svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15 27" enable-background="new 0 0 15 27" xml:space="preserve"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z"/></svg></button>',
+		nextArrow: '<button type="button" class="slider_arrow slider_arrow-next"><svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15 27" enable-background="new 0 0 15 27" xml:space="preserve"><path fill-rule="evenodd" clip-rule="evenodd" d="M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z"/></svg></button>'
+	});
+	$('.slider-init-2_2').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.slider-init-2_1',
+		dots: false,
+		arrows: false,
+		focusOnSelect: true,
+		fade: true
+	});
+
 
 	$('#menu').on('click', function() {
 		$('.menu_mobile').toggleClass('menu_mobile-close');
@@ -35,7 +54,7 @@ $(document).ready(function() {
 	$('#work_list').on('click', function() {
 		dropLinks('#work_list-open', 'read-other__open')
 	});
-
+	ymaps.ready(initYandexMap);
 });
 
 function dropLinks(click, toggleClass)  {
